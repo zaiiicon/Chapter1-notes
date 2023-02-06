6bd30669cd79c69c1869eed272aa552b273da380
8t8f <8t8f@fresnou.org>
2023-02-06 13:58:11 -0800

    initial commit

---
+++ Chapter1.js
@@ -0,0 +1,35 @@
+console.log(7);
+//This is a comment
+
+//Numbers
+//Integers
+console.log(-3);
+console.log(9);
+console.log(0);
+
+//Floating Point Numbers (Floats)
+console.log(3.14);
+console.log(-8.123456);
+
+//Strings
+//Characters, letters, symbols, spaces
+console.log("Life");
+console.log('!@#$ ps also a string');
+console.log(`Backticks by the 1`);
+
+//Arithmetic Operators (Math)
+console.log(14+2);
+console.log(14-2);
+console.log(14*2); //shift-8 gives *
+console.log(14/2);
+console.log(14%2); //% is called MOD
+                   //it means reminder after dividing
+
+//Concatenation means to combine strings
+console.log("My name "+" is Zai.");
+console.log('My'+' '+'age'+' '+'is '+14+'.' );
+console.log("Your age is /n16.");
+
+//Template literal - plug in some calculated vale
+//Use backticks `` by the 1 on your keyboard
+console.log(`15 divided by 10 = ${15/10}`);
---
+++ Operators.js
@@ -0,0 +1,62 @@
+// Unary Operators use one value
+console.log(typeof 15);
+console.log(typeof"this string");
+console.log();
+console.log();
+
+// Binary Operators use two values
+// Comparison Operators
+
+// Binary Operators use two values
+
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+
+// Logical Operators
+// AND && shift-7 true when both are true
+
+console.log();
+console.log();
+console.log();
+console.log();
+
+// OR || shift-\ (next to enter key)
+//true when at least one was true
+
+console.log();
+console.log();
+console.log();
+console.log();
+
+// NOT ! flips true to false and false to true
+
+console.log();
+console.log();
+
+// Ternary Operators use three values
+
+console.log();
+console.log();
+console.log();
+console.log();
+
+// Order of Operations
+// Everything Else (happens first)
+// Comparison Operators
+// AND
+// OR (happens last)
+
+console.log();
+
+// 2 == 3 && 120 > 100
+// false and true
+// false
+
+console.log();
+console.log();
